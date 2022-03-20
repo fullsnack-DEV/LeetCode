25bc9a72be8f623bb9dc22433507954f5ddc0aad
Pratik Patre <56273302+fullsnack-DEV@users.noreply.github.com>
2022-03-20 21:43:57 +0530

    Time: 133 ms (49.06%), Space: 49 MB (77.42%) - LeetHub

---
+++ 344-reverse-string/344-reverse-string.js
@@ -0,0 +1,19 @@
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+var reverseString = function(s) {
+  
+    let first = 0;
+  let last = s.length - 1;
+
+  while (first <= last) {
+    let temp = s[first];
+    s[first] = s[last];
+    s[last] = temp;
+    last--;
+    first++;
+  }
+  return s;
+    
+};
